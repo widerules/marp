5db6987496865f7a1662356429a715266b3bc530
vizrnld@gmail.com <vizrnld@gmail.com@5c3d5193-88ce-f85a-b894-451a9150f4fe>
2012-05-30 07:30:04 +0000

    osszes metodus megirasa

--- trunk/MARPAndroid/src/edu/ubb/marp/ui/ListRecord.java
+++ trunk/MARPAndroid/src/edu/ubb/marp/ui/ListRecord.java
@@ -8,4 +8,20 @@ public class ListRecord {
 	        this.item = item;
 	        this.subitem = subitem;
 	    }
+	    public ListRecord(){
+	    	this.item = "";
+	    	this.subitem = "";
+	    }
+	    public String getItem(){
+	    	return this.item;
+	    }
+	    public String getSubitem(){
+	    	return this.subitem;
+	    }
+	    public void setItem(String s){
+	    	this.item = s;
+	    }
+	    public void setStubitem(String s){
+	    	this.subitem = s;
+	    }
 }
